a5cc7204ceb7e53dd150af33ac39c82a7017fa74
Seungyeon Lee <39074545+yeon-lab@users.noreply.github.com>
2022-05-12 16:22:49 -0400

    Update metric.py

--- model/metric.py
+++ model/metric.py
@@ -2,13 +2,6 @@ import torch
 from sklearn.metrics import confusion_matrix, f1_score, accuracy_score
 
 
-#def accuracy(output, target):
-#    with torch.no_grad():
-#        pred = torch.argmax(output, dim=1)
-#        assert pred.shape[0] == len(target)
-#        correct = 0
-#        correct += torch.sum(pred == target).item()
-#    return correct / len(target)
 def accuracy(output, target):
     return accuracy_score(target, output)
 
@@ -16,8 +9,4 @@ def confusion(output, target):
     return confusion_matrix(target,output )
 
 def f1(output, target):
-    #with torch.no_grad():
-    #    pred = torch.argmax(output, dim=1)
-    #assert output.shape[0] == len(target)
     return f1_score(target,output, average='macro')
-#    return f1_score(target.data.cpu().numpy(), pred.cpu().numpy(),  average='macro')
