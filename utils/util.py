330de591c3db2d0cb1363ce0e8622602e791392a
Seungyeon Lee <39074545+yeon-lab@users.noreply.github.com>
2022-06-03 12:55:06 -0400

    Update util.py

--- utils/util.py
+++ utils/util.py
@@ -95,6 +95,7 @@ def load_edf_folds(np_data_path, n_folds, idx):
 
     return folds_data
 
+
 def load_edf_folds_proposed_method(np_data_path, n_folds, idx):
     np.random.seed(SEED)
     
@@ -135,8 +136,10 @@ def load_edf_folds_proposed_method(np_data_path, n_folds, idx):
             valid_list = valid_1+valid_2
             train_list = splited_files[n_valid-len(valid_1):fold_id]
         
-        valid_file = [sum(sublist.tolist(),[]) for sublist in valid_list]
-        train_file = [sum(sublist.tolist(),[]) for sublist in train_list]
+        valid_file = [sublist.tolist() for sublist in valid_list]
+        train_file = [sublist.tolist() for sublist in train_list]  
+        valid_file = sum(valid_file,[])
+        train_file = sum(train_file,[])
             
         folds_data[fold_id] = {'train': train_file, 
                                'valid': valid_file, 
@@ -154,6 +157,72 @@ def load_edf_folds_proposed_method(np_data_path, n_folds, idx):
           'n train:', len(train), 'n valid:', len(valid),'n test:', len(test))
 
     return folds_data
+
+
+
+def load_folds_semi_spervised(n_folds, idx):
+    np.random.seed(SEED)
+    
+    edf_20_files = sorted(glob(os.path.join('data_npz/edf_20_fpzcz', "*.npz")))
+    edf_78_files = sorted(glob(os.path.join('data_npz/edf_78_fpzcz', "*.npz")))
+    
+    n_valid = 4
+
+    files_dict = dict()
+    for i in edf_20_files:
+        file_name = os.path.split(i)[-1] 
+        file_num = file_name[3:5]
+        if file_num not in files_dict:
+            files_dict[file_num] = [i]
+        else:
+            files_dict[file_num].append(i)
+    
+    files_pairs = []
+    for i, key in enumerate(files_dict):
+        files_pairs.append(files_dict[key])
+    np.random.shuffle(files_pairs)
+    splited_files = np.array_split(files_pairs, n_folds)
+    
+    
+    train_edf_78 = list()        
+    for i in edf_78_files:
+        file_name = os.path.split(i)[-1] 
+        file_num = file_name[3:5]
+        if file_num not in files_dict:
+            train_edf_78.append(i)
+
+    folds_data = {}
+    for fold_id in range(n_folds):
+        test_file = sum(splited_files[fold_id].tolist(),[])
+        
+        if fold_id+n_valid < n_folds:   
+            valid_list = splited_files[fold_id+1:fold_id+1+n_valid]
+        else: 
+            valid_1 = splited_files[fold_id+1:]
+            valid_2 = splited_files[:n_valid-len(valid_1)]
+            valid_list = valid_1+valid_2
+        
+        valid_file = [sum(sublist.tolist(), []) for sublist in valid_list]
+        valid_file = sum(valid_file,[])
+        
+        train_edf_20 = list(set(edf_20_files) - set(valid_file) -set(test_file))
+                
+        folds_data[fold_id] = {'train_sup': train_edf_20, 
+                               'train_unsup': train_edf_78,
+                               'valid': valid_file, 
+                               'test': test_file}
+    
+    train_sup = folds_data[idx]['train_sup']
+    train_unsup = folds_data[idx]['train_unsup']
+    valid = folds_data[idx]['valid']
+    test = folds_data[idx]['test']
+
+    all_ = np.concatenate((train_sup,train_unsup,valid,test))
+    print('\n n data:',len(np.unique(all_)), 'sum:', len(train_sup)+len(train_unsup)+len(valid)+len(test), 
+          'n_train_sup:', len(train_sup), ' n_train_unsup:', len(train_unsup), ' n valid:', len(valid),' n test:', len(test))
+
+    return folds_data
+
 
 
 torch.manual_seed(SEED)
