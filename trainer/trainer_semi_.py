09e71829295b6141ae51ad37aec32b78d8a45cff
Seungyeon <39074545+yeon-lab@users.noreply.github.com>
2023-09-28 12:05:24 -0400

    Update trainer_semi_.py

--- trainer/trainer_semi_.py
+++ trainer/trainer_semi_.py
@@ -10,11 +10,13 @@ class Trainer(BaseTrainer):
     Trainer class
     """
     def __init__(self, feature_net, classifier, featurenet_optimizer, classifier_optimizer, 
-                 criterion, metric_ftns, config, data_loader, fold_id, 
+                 criterion, metric_ftns, config, fold_id, supervised_loader, unsupervised_loader,
                  valid_loader=None, test_loader=None, class_weights=None, reduce_lr=False):
         super().__init__(feature_net, classifier, featurenet_optimizer, classifier_optimizer, 
                          criterion, metric_ftns, config, fold_id)
-        self.data_loader = data_loader
+        self.supervised_loader = supervised_loader
+        self.unsupervised_loader = unsupervised_loader
+        self.n_trains_featurenet = len(supervised_loader)+len(unsupervised_loader)
 
         self.valid_loader = valid_loader
         self.do_validation = self.valid_loader is not None
@@ -22,47 +24,78 @@ class Trainer(BaseTrainer):
         self.do_test = self.test_loader is not None
         self.lr_scheduler_f = featurenet_optimizer
         self.lr_scheduler_c = classifier_optimizer
-        self.log_step = int(data_loader.batch_size) * 1  # reduce this if you want more logs
+        self.log_step = int(supervised_loader.batch_size) * 1  # reduce this if you want more logs
 
         self.metrics = MetricTracker('loss', *[m.__name__ for m in self.metric_ftns])
 
         self.class_weights = class_weights
         self.reduce_lr = reduce_lr
-        
+
     def _train_feature_net(self, epoch):
         self.feature_net.train()
         self.metrics.reset()
         
         self.feature_net.beta_d = min([self.config['hyper_params']['beta_d'], self.config['hyper_params']['beta_d'] * (epoch * 1.) / self.config['hyper_params']['warmup']])
         self.feature_net.beta_y = min([self.config['hyper_params']['beta_y'], self.config['hyper_params']['beta_y'] * (epoch * 1.) / self.config['hyper_params']['warmup']])
+        print('beta d:', self.feature_net.beta_d)
+        print('beta y:', self.feature_net.beta_y)
 
-        outs = np.array([]), trgs = np.array([])
-        for batch_idx, (x, y, d) in enumerate(self.data_loader):
+        outs, trgs = np.array([]), np.array([])
+        self_supervised_loss = torch.tensor(0)
+        
+        sup_iter = iter(self.supervised_loader)
+        unsup_iter = iter(self.unsupervised_loader)
+        
+        is_supervised = True
+        for iter_ in range(self.n_trains_featurenet):
+            if iter_ % 2 == 0 and is_supervised: 
+                try:
+                    (x, y, d) = next(sup_iter)
+                    phase = 'Supervised learning'
+                except:
+                    (x, y, d) = next(unsup_iter)
+                    phase = 'UnSupervised learning'
+                    is_supervised = False
+            else:
+                (x, y, d) = next(unsup_iter)
+                phase = 'UnSupervised learning'
+            
             x, y, d = x.to(self.device), y.to(self.device), d.to(self.device)
+
             self.featurenet_optimizer.zero_grad()
-            all_loss = self.feature_net.get_losses(x, y, d)
-            output = self.feature_net.predict(x)
-            loss = self.criterion(output, y, self.class_weights, self.device)
+            
+            if iter_ % 2 == 0 and is_supervised:
+                all_loss, DIVA_loss, CE_class, CE_domain, Conts_loss, KL_domain, KL_class, Recon_loss = self.feature_net.get_losses(x=x, y=y, d=d)
+            else: 
+                all_loss, DIVA_loss, CE_class, CE_domain, Conts_loss, KL_domain, KL_class, Recon_loss = self.feature_net.get_losses(x=x, y=None, d=d)
+                
             all_loss.backward()
             self.featurenet_optimizer.step()
+            
+            if iter_ % 2 == 0 and is_supervised:
+                output = self.feature_net.predict(x)
+                loss = self.criterion(output, y, self.class_weights, self.device)
+                self.train_metrics.update('loss', loss.item())
+                
+                preds_ = output.data.max(1, keepdim=True)[1].cpu()    
+                outs = np.append(outs, preds_.numpy())
+                trgs = np.append(trgs, y.data.cpu().numpy())
 
-            self.metrics.update('loss', loss.item())
-
-            if batch_idx % self.log_step == 0:
-                self.logger.debug('Train Epoch: {} {} Loss: {:.6f} ClassLoss: {:.6f} ContsLoss: {:.6f}'.format(
+            if iter_ % self.log_step == 0:
+                self.logger.debug('Train Epoch: {} {}  {} - Loss: {:.6f} ClassLoss: {:.6f} ContLoss: {:.6f}'.format(
                     epoch,
-                    self._progress(batch_idx),
+                    self._progress(iter_),
+                    phase,
                     all_loss.item(),
                     loss.item(),
                     Conts_loss.item()
                 ))
-            preds_ = output.data.max(1, keepdim=True)[1].cpu()    
-            outs = np.append(outs, preds_.numpy())
-            trgs = np.append(trgs, y.data.cpu().numpy())
-                            
+  
         for met in self.metric_ftns:
-            self.metrics.update(met.__name__, met(outs.reshape(-1,1), trgs.reshape(-1,1)))      
+            self.metrics.update(met.__name__, met(outs.reshape(-1,1), trgs.reshape(-1,1)))
+            
         log = self.metrics.result()
+
 
         if self.do_validation:
             val_log = self._infer_feature_net(self.valid_loader)
@@ -125,7 +158,7 @@ class Trainer(BaseTrainer):
         self.metrics.reset()
 
         outs, trgs = np.array([]), np.array([])
-        for batch_idx, (x, y, _) in enumerate(self.data_loader):
+        for batch_idx, (x, y, _) in enumerate(self.supervised_loader):
             x, y = x.to(self.device), y.to(self.device)
             self.classifier_optimizer.zero_grad()
             features = self.feature_net.get_features(x)
