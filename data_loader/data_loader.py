9deb31cab503798e67d4f5c4e0ad8ef15cad043e
Seungyeon <39074545+yeon-lab@users.noreply.github.com>
2023-10-05 21:04:40 -0400

    Update data_loader.py

--- data_loader/data_loader.py
+++ data_loader/data_loader.py
@@ -5,19 +5,16 @@ from torch.utils.data import Dataset
 
 
 class SleepDataLoader(Dataset):
-    def __init__(self, config, files, d_type, phase):
+    def __init__(self, config, files, phase):
         self.seq_len = config['hyper_params']['seq_len']
         self.files = files
         self.counts = None
         self.phase = phase
-        self.n_domains = 0
-        self.n_data = 0
-        self.check_shape = True
+        self.n_domains = None
+        self.n_data = None
+        self.domain_idx = 0
         
-        if d_type == 'edf':
-            self.inputs, self.labels, self.epochs = self.split_dataset_edf()
-        elif d_type == 'shhs':
-            self.inputs, self.labels, self.epochs = self.split_dataset()
+        self.inputs, self.labels, self.epochs = self.split_dataset()
             
         print(phase+' info:')
         if self.n_data == len(self.inputs) and self.n_data == len(self.labels):
@@ -31,55 +28,18 @@ class SleepDataLoader(Dataset):
 
     def __getitem__(self, idx):
         file_idx, domain_idx, idx, seq_len = self.epochs[idx]
-        
         inputs = self.inputs[file_idx][idx*seq_len:(idx+1)*seq_len]
         inputs = torch.from_numpy(inputs).float()
-        
         labels = self.labels[file_idx][idx*seq_len:(idx+1)*seq_len]
         labels = torch.from_numpy(labels).long()
-        
-        if self.check_shape and self.phase == 'train':
-            print('\nx shape: {}'.format(inputs.shape))
-            print('y shape: {}\n'.format(labels.shape))
-            print('-'*100)
-            self.check_shape = False
-
         return inputs, labels, domain_idx
-            
-
-    def split_dataset(self):
-
-        inputs, labels, epochs = [], [], []
-        all_ys = np.array([])
-
-        for file_idx, file in enumerate(self.files):
-            npz_file = np.load(file)
-            inputs.append(npz_file['x'])
-            labels.append(npz_file['y'])
-            all_ys = np.append(all_ys, npz_file['y'])
-            
-            epoch_size = len(npz_file['x']) // self.seq_len
-            for i in range(epoch_size):
-                epochs.append([file_idx, file_idx, i, self.seq_len])
-                
-        if self.phase == 'train':
-            all_ys = all_ys.tolist()
-            num_classes = len(np.unique(all_ys))
-            counts = [all_ys.count(i) for i in range(num_classes)]
-            self.counts = counts
-            
-        self.n_domains = file_idx+1
-        self.n_data = file_idx+1
-
-        return inputs, labels, epochs
         
-    def split_dataset_edf(self):
-
+    def split_dataset(self):
         inputs, labels, epochs = [], [], []
         all_ys = np.array([])
         
         file_idx = 0
-        for domain_idx, file_list in enumerate(self.files):
+        for file_list in self.files:
             for file in file_list:
                 npz_file = np.load(file)
                 inputs.append(npz_file['x'])
@@ -89,17 +49,16 @@ class SleepDataLoader(Dataset):
                 epoch_size = len(npz_file['x']) // self.seq_len
                 for i in range(epoch_size):
                     epochs.append([file_idx, domain_idx, i, self.seq_len])
-                    
                 file_idx += 1
+            self.domain_idx += 1
 
-                
         if self.phase == 'train':
             all_ys = all_ys.tolist()
             num_classes = len(np.unique(all_ys))
             counts = [all_ys.count(i) for i in range(num_classes)]
             self.counts = counts
             
-        self.n_domains = domain_idx+1
+        self.n_domains = self.domain_idx
         self.n_data = file_idx
         
         return inputs, labels, epochs
