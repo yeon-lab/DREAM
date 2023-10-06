97f995e16920aba6f2356d04b4e574995907a64c
Seungyeon <39074545+yeon-lab@users.noreply.github.com>
2023-10-05 20:55:55 -0400

    Update train.py

--- train.py
+++ train.py
@@ -21,7 +21,7 @@ torch.manual_seed(SEED)
 torch.backends.cudnn.deterministic = False
 torch.backends.cudnn.benchmark = False
 
-def main(config, fold_id, data_config):
+def main(config, fold_id, sampling_rate):
     logger = config.get_logger('train') 
     logger.info('='*100)
     logger.info("fold id:{}".format(fold_id))
@@ -30,11 +30,11 @@ def main(config, fold_id, data_config):
     batch_size = config["data_loader"]["args"]["batch_size"]
     params = config['hyper_params']
     
-    train_dataset = SleepDataLoader(config, folds_data[fold_id]['train'], d_type=data_config['d_type'], phase='train')
+    train_dataset = SleepDataLoader(config, folds_data[fold_id]['train'], phase='train')
     data_loader = DataLoader(dataset=train_dataset, shuffle=True, batch_size = batch_size)
-    valid_dataset = SleepDataLoader(config, folds_data[fold_id]['valid'], d_type=data_config['d_type'], phase='valid')
+    valid_dataset = SleepDataLoader(config, folds_data[fold_id]['valid'], phase='valid')
     valid_loader = DataLoader(dataset=valid_dataset, shuffle=True, batch_size = batch_size) 
-    test_dataset = SleepDataLoader(config, folds_data[fold_id]['test'], d_type=data_config['d_type'], phase='test')
+    test_dataset = SleepDataLoader(config, folds_data[fold_id]['test'], phase='test')
     test_loader = DataLoader(dataset=test_dataset, shuffle=True, batch_size = batch_size) 
     logger.info("-"*100)
         
@@ -43,7 +43,7 @@ def main(config, fold_id, data_config):
     
     # build model architecture, initialize weights, then print to console  
    
-    feature_net = VAE(params['zd_dim'] , params['zy_dim'], n_domains, config, data_config['d_type']) 
+    feature_net = VAE(params['zd_dim'] , params['zy_dim'], n_domains, config, sampling_rate) 
     classifier = Transformer(input_size=params['zy_dim'], config=config) 
 
     logger.info(feature_net)
@@ -62,14 +62,6 @@ def main(config, fold_id, data_config):
     featurenet_optimizer = config.init_obj('optimizer', torch.optim, featurenet_parameters)
     classifier_optimizer = config.init_obj('optimizer', torch.optim, classifier_parameters)
     
-    try:
-        reduce_lr = params['reduce_lr']
-        print('reduce_lr is applied.')
-    except:
-        reduce_lr = False
-        print('reduce_lr is not applied.')
-    
-    print('reduce_lr:', reduce_lr)
     trainer = Trainer(feature_net, classifier, 
                       featurenet_optimizer, classifier_optimizer,
                       criterion, metrics,
@@ -78,8 +70,7 @@ def main(config, fold_id, data_config):
                       fold_id=fold_id,
                       valid_loader=valid_loader,
                       test_loader=test_loader,
-                      class_weights=weights_for_each_class,
-                      reduce_lr=reduce_lr)
+                      class_weights=weights_for_each_class)
 
     trainer.train()
 
@@ -106,12 +97,10 @@ if __name__ == '__main__':
     data_config = dict()
     if "shhs" in args2.np_data_dir:
         folds_data = load_shhs_folds(args2.np_data_dir, config["data_loader"]["args"]["num_folds"], fold_id)
-        data_config['d_type'] = 'shhs'
-        data_config['sampling_rate'] = 125
+        sampling_rate = 125
     else:
         folds_data = load_edf_folds(args2.np_data_dir, config["data_loader"]["args"]["num_folds"], fold_id)
-        data_config['d_type'] = 'edf'
-        data_config['sampling_rate'] = 100
+        sampling_rate = 100
     
 
-    main(config, fold_id, data_config)
+    main(config, fold_id, sampling_rate)
