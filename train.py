d10ffa6a2e2f507e14cb6abb7942f8622b1d7d45
Seungyeon <39074545+yeon-lab@users.noreply.github.com>
2023-11-27 22:13:52 -0500

    Update train.py

--- train.py
+++ train.py
@@ -11,6 +11,7 @@ from utils.util import *
 from model.DREAM import *
 
 import torch
+import torch.nn as nn
 from torch.utils.data import DataLoader
 
 
@@ -22,6 +23,9 @@ torch.backends.cudnn.benchmark = False
 
 def main(config, fold_id, sampling_rate):
     logger = config.get_logger('train') 
+    logger.info('='*100)
+    logger.info("fold id:{}".format(fold_id))
+    logger.info('-'*100)
     
     batch_size = config["data_loader"]["args"]["batch_size"]
     params = config['hyper_params']
@@ -38,9 +42,8 @@ def main(config, fold_id, sampling_rate):
     n_domains = train_dataset.n_domains
     
     # build model architecture, initialize weights, then print to console  
-   
-    feature_net = VAE(params['zd_dim'], params['zy_dim'], n_domains, config, sampling_rate) 
-    classifier = Transformer(input_size=params['zy_dim'], config=config) 
+    feature_net = VAE(config, n_domains, sampling_rate, augment=False) 
+    classifier = Transformer(config) 
 
     # get function handles of loss and metrics
     criterion = getattr(module_loss, config['loss'])
@@ -52,7 +55,7 @@ def main(config, fold_id, sampling_rate):
 
     featurenet_optimizer = config.init_obj('optimizer', torch.optim, featurenet_parameters)
     classifier_optimizer = config.init_obj('optimizer', torch.optim, classifier_parameters)
-    
+
     trainer = Trainer(feature_net, classifier, 
                       featurenet_optimizer, classifier_optimizer,
                       criterion, metrics,
@@ -62,9 +65,7 @@ def main(config, fold_id, sampling_rate):
                       valid_loader=valid_loader,
                       test_loader=test_loader,
                       class_weights=weights_for_each_class)
-
     trainer.train()
-
 
 if __name__ == '__main__':
     args = argparse.ArgumentParser(description='PyTorch Template')
@@ -77,15 +78,12 @@ if __name__ == '__main__':
     args.add_argument('-da', '--np_data_dir', type=str,
                       help='Directory containing numpy files')
 
-
     CustomArgs = collections.namedtuple('CustomArgs', 'flags type target')
 
     args2 = args.parse_args()
     fold_id = int(args2.fold_id)
-
     config = ConfigParser.from_args(args, fold_id)
     
-    data_config = dict()
     if "shhs" in args2.np_data_dir:
         folds_data = load_shhs_folds(args2.np_data_dir, config["data_loader"]["args"]["num_folds"], fold_id)
         sampling_rate = 125
