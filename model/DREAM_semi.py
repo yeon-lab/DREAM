76277e1154671dab1e9bf47aa35114f8aa2a5602
Seungyeon <39074545+yeon-lab@users.noreply.github.com>
2023-11-27 22:13:07 -0500

    Update DREAM_semi.py

--- model/DREAM_semi.py
+++ model/DREAM_semi.py
@@ -6,7 +6,7 @@ import random
 from TorchCRF import CRF
 
 import torch
-from model.base import Decoder_ResNet, Encoder_ResNet, p_decoder, aux_layer
+from model.base import Decoder_ResNet, Encoder_ResNet, p_decoder, aux_layer, Augmentation
 from model.loss import SupervisedContrastiveLoss, Self_SupervisedContrastiveLoss
 SEED = 1111
 torch.manual_seed(SEED)
@@ -14,22 +14,18 @@ torch.backends.cudnn.deterministic = False
 torch.backends.cudnn.benchmark = False
 
 class VAE(nn.Module):
-    def __init__(self, zd_dim, zy_dim, n_domains, config, d_type):
+    def __init__(self, config, n_domains, sampling_rate):
         super(VAE, self).__init__()
-        self.zd_dim = zd_dim
-        self.zy_dim = zy_dim
         self.d_dim = n_domains
         params = config['hyper_params']
+        self.zd_dim = params['zd_dim']
+        self.zy_dim = params['zy_dim']
         self.y_dim = params['num_classes']
         self.seq_len = params['seq_len']
-        
-        if d_type == 'edf':
-            self.sampling_rate = 100
-        elif d_type == 'shhs':
-            self.sampling_rate = 125
+        self.sampling_rate = sampling_rate
             
         self.contrastive_loss = SupervisedContrastiveLoss()
-        self.transformer= Transform()
+        self.augmentor = Augmentation(sampling_rate=sampling_rate)
         self.criterion = nn.CrossEntropyLoss()
             
         self.px = Decoder_ResNet(self.zd_dim, self.zy_dim, self.sampling_rate)
@@ -46,17 +42,17 @@ class VAE(nn.Module):
         self.aux_loss_multiplier_y = params['aux_loss_y']
         self.aux_loss_multiplier_d = params['aux_loss_d']
 
-        self.beta_x, self.beta_y, self.beta_d = params['beta_x'], params['beta_y'], params['beta_d']
-        self.const_weight = config['hyper_params']['const_weight']
+        self.beta_y, self.beta_d = params['beta_y'], params['beta_d']
+        self.const_weight = params['const_weight']
 
         
     def forward(self, x, y, d):
-        zd_q_loc, zd_q_scale = self.qzd(x)        # Encode
-        qzd = dist.Normal(zd_q_loc, zd_q_scale)   # Reparameterization trick
-        zd_q = qzd.rsample() 
+        zd_q_loc, zd_q_scale = self.qzd(x)
+        zy_q_loc, zy_q_scale = self.qzy(x)
 
-        zy_q_loc, zy_q_scale = self.qzy(x)          # Encode
-        qzy = dist.Normal(zy_q_loc, zy_q_scale)     # Reparameterization trick
+        qzd = dist.Normal(zd_q_loc, zd_q_scale)
+        zd_q = qzd.rsample()
+        qzy = dist.Normal(zy_q_loc, zy_q_scale)
         zy_q = qzy.rsample()
 
         # Decode
@@ -86,13 +82,13 @@ class VAE(nn.Module):
         return x_recon, d_hat, y_hat, qzd, pzd, zd_q, qzy, pzy, zy_q, zy_q_loc
 
     def get_losses(self, x, y, d):        
-        DIVA_losses, conts_losses, CE_class, CE_domain = 0, 0, 0, 0
+        VAE_losses, conts_losses = 0, 0
         
         d_target = d
         d_input = F.one_hot(d, num_classes= self.d_dim).float()
 
         for i in range(self.seq_len):
-            x_input = x[:, i].view(x.size(0),1, -1)
+            x_sample = x[:, i].view(x.size(0),1, -1)                  
             if y is not None:
                 y_target = y[:, i]
                 y_input = F.one_hot(y_target, num_classes= self.y_dim).float() 
@@ -101,13 +97,12 @@ class VAE(nn.Module):
     
                 CE_x = F.mse_loss(x_recon, x_input, reduction='sum')
                 CE_d = F.cross_entropy(d_hat, d_target, reduction='sum')
-    
                 zd_p_minus_zd_q = torch.sum(pzd.log_prob(zd_q) - qzd.log_prob(zd_q))
                 zy_p_minus_zy_q = torch.sum(pzy.log_prob(zy_q) - qzy.log_prob(zy_q))
             
                 CE_y = F.cross_entropy(y_hat, y_target, reduction='sum')
                 
-                DIVA_losses += CE_x \
+                VAE_losses += CE_x \
                    - self.beta_d * zd_p_minus_zd_q \
                    - self.beta_y * zy_p_minus_zy_q \
                    + self.aux_loss_multiplier_d * CE_d \
@@ -117,17 +112,15 @@ class VAE(nn.Module):
                 
             else:
                 y_input = None
-                
-                
-                x_croped = self.transformer.crop_resize(x_input,random.uniform(0.25,0.75))
-                x_croped  = torch.FloatTensor(x_croped).cuda()
-                
-                x_permuted = self.transformer.permute(x_input,random.randint(5,20))
-                x_permuted  = torch.FloatTensor(x_permuted).cuda()
+                x_croped = self.augmentor.crop_resize(x_sample,random.uniform(0.25,0.75))
+                x_croped  = torch.FloatTensor(x_croped).to(y.device)
+                x_permuted = self.augmentor.permute(x_sample,random.randint(5,20))
+                x_permuted  = torch.FloatTensor(x_permuted).to(y.device)
+                x_set = [x_croped, x_permuted]
                  
                 feature_set = torch.tensor([]).cuda()
-                for x_transformed in [x_croped, x_permuted]:
-                    x_recon, d_hat, y_hat, qzd, pzd, zd_q, qzx, pzx, zx_q, qzy, pzy, zy_q, features = self.forward(x=x_transformed, y=y_input, d=d_input)
+                for x_transformed in x_set:
+                    x_recon, d_hat, y_hat, qzd, pzd, zd_q, qzy, pzy, zy_q, features = self.forward(x=x_transformed, y=y_input, d=d_input)
                     
                     feature_set = torch.cat((feature_set,features), dim=0)
                     
@@ -135,12 +128,7 @@ class VAE(nn.Module):
                     CE_d = F.cross_entropy(d_hat, d_target, reduction='sum')
         
                     zd_p_minus_zd_q = torch.sum(pzd.log_prob(zd_q) - qzd.log_prob(zd_q))
-                    
-                    if self.zx_dim != 0:
-                        KL_zx = torch.sum(pzx.log_prob(zx_q) - qzx.log_prob(zx_q))
-                    else:
-                        KL_zx = 0
-                        
+
                     y_onehot = torch.eye(self.y_dim)
                     y_onehot = y_onehot.repeat(1, 100)
                     y_onehot = y_onehot.view(-1, self.y_dim).cuda()
@@ -157,21 +145,14 @@ class VAE(nn.Module):
                     prior_y_minus_qy = torch.log(prior_y) - qy.log_prob(y_onehot)
                     marginal_prior_y_minus_qy = torch.sum(prob_qy * prior_y_minus_qy)
         
-                    DIVA_losses += (CE_x \
+                    VAE_losses += (CE_x \
                            - self.beta_d * zd_p_minus_zd_q \
-                           - self.beta_x * KL_zx \
                            - self.beta_y * marginal_zy_p_minus_zy_q \
                            - marginal_prior_y_minus_qy \
                            + self.aux_loss_multiplier_d * CE_d)*0.5                      
-           
-                    
                 conts_losses += Self_SupervisedContrastiveLoss(feature_set, self.criterion)*self.const_weight
-                
 
-        all_losses = (DIVA_losses+conts_losses)/self.seq_len
-        
-
-        return all_losses, DIVA_losses/self.seq_len, CE_class/self.seq_len, CE_domain/self.seq_len, conts_losses/self.seq_len, KL_domain/self.seq_len, KL_class/self.seq_len, reconst_losses/self.seq_len
+        return (VAE_losses+conts_losses)/self.seq_len
 
   
     def get_features(self, x):
@@ -181,84 +162,60 @@ class VAE(nn.Module):
             x_input = x[:, i].view(batch_size, 1, -1)
             features, _ = self.qzy.forward(x_input)
             f_seq.append(features.view(batch_size,1, -1))
-
         out = torch.cat(f_seq, dim=1)
         return out # (batch_size,len, n_feat)   
-    
-    
+
     def predict(self, x):
         batch_size = x.size(0)
         f_seq = []
         with torch.no_grad():
             for i in range(self.seq_len):   
                 x_input = x[:, i].view(batch_size, 1, -1)
-                features, _ = self.qzy.forward(x_input)
-                alpha = F.softmax(self.qy(features), dim=1)
+                zy, _ = self.qzy.forward(x_input)
+                alpha = F.softmax(self.qy(zy), dim=1)
                 res, ind = torch.topk(alpha, 1)
                 y = x_input.new_zeros(alpha.size())
                 y = y.scatter_(1, ind, 1.0)
                 f_seq.append(y.view(batch_size,1, -1))
-                
             out = torch.cat(f_seq, dim=1)
             out = out.permute(0,2,1)           # (batch_size, n_class, len)      
         return out
     
 
-    
-class Transformer(nn.Module):
-    def __init__(self, input_size, config, n_layer=4, n_classes=5):
-        super(Transformer, self).__init__()
-        SEED = 1111
-        torch.manual_seed(SEED)
-        torch.backends.cudnn.deterministic = False
-        torch.backends.cudnn.benchmark = False
-        self.hidden_dim = input_size
-        self.batch_size = config["data_loader"]["args"]["batch_size"]
-        self.dim_feedforward = config['hyper_params']['dim_feedforward']
-        self.is_CFR =  config['hyper_params']['is_CFR']
-        try:
-            self.n_layer = config['hyper_params']['n_layers']
-        except:
-            self.n_layer = n_layer
+################################################################################
+################################################################################
 
-        if self.is_CFR  is True:
-            self.mask = torch.ones((self.batch_size, config['hyper_params']['seq_len'])).byte().cuda()
-            self.crf = CRF(n_classes)
-        else: 
-            self.criterion = nn.CrossEntropyLoss()
-            self.softmax = nn.Softmax(dim=2) 
-            
+class Transformer(nn.Module):
+    def __init__(self, config, n_layer=4, n_classes=5):
+        super(Transformer, self).__init__()
+        params = config['hyper_params']
+        self.hidden_dim = params['zy_dim']
+        self.batch_size = config["data_loader"]["args"]["batch_size"]
+        self.dim_feedforward = params['dim_feedforward']
+        self.n_layer = params['n_layers']
+        
+        self.mask = torch.ones((self.batch_size, params['seq_len'])).byte().cuda()
+        self.crf = CRF(n_classes)
+
         self.encoder_layer = nn.TransformerEncoderLayer(d_model=self.hidden_dim, nhead=8, batch_first=True, dim_feedforward=self.dim_feedforward) 
         self.transformer_encoder = nn.TransformerEncoder(self.encoder_layer, num_layers=self.n_layer)
         self.fc = nn.Linear(self.hidden_dim, n_classes)
         
     def forward(self, x): #in: (batch, seq, feature) if batch_first=True
-        #out:(batch_size, seq_len, feature).
         x = self.transformer_encoder(x)
         x = self.fc(x)
         return x  # (N_batch, Length, Class)
         
     def get_loss(self, x, y): 
         x = self.forward(x)  # out: (N_batch, Length, Class)
-        if self.is_CFR is True:
-            mask = self.mask[:len(y)]
-            loss = self.crf.forward(x, y, mask)  # y: (batch_size, sequence_size), mask: (batch_size, sequence_size), out: (batch_size, sequence_size, num_labels)
-            loss = -loss.mean()
-        else:
-            x = self.softmax(x)  # (N_batch, Length, Class)
-            x = x.permute(0,2,1) # (N_batch, Class, L)
-            loss =  self.criterion(x, y) # input:(N, C, L), out:(N,L)
-            
+        mask = self.mask[:len(y)]
+        loss = self.crf.forward(x, y, mask)  # y: (batch_size, sequence_size), mask: (batch_size, sequence_size), out: (batch_size, sequence_size, num_labels)
+        loss = -loss.mean()
         return loss
     
     def predict(self, x):
         x = self.forward(x) # out: (N_batch, Length, Class)
-        if self.is_CFR is True:
-            mask = self.mask[:len(x)]
-            x = self.crf.viterbi_decode(x, mask)
-        else:
-            x = self.softmax(x)  # (N_batch, Length, Class)
-            x = x.permute(0,2,1) # (N_batch, Class, L)
-            x = x.data.max(1)[1].cpu()
-        return x
+        mask = self.mask[:len(x)]
+        y = self.crf.viterbi_decode(x, mask)
+        return y
        
